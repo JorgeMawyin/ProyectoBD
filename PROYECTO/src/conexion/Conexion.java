326166a3ed40ee0669af1e56fecaaaf95703f570
JorgeMawyin <jmawyin@espol.edu.ec>
2022-12-27 14:09:15 -0500

    proyecto base

---
+++ PROYECTO/src/conexion/Conexion.java
@@ -0,0 +1,18 @@
+package conexion;
+
+import java.sql.*;
+
+public class Conexion {
+
+    Connection cn;
+    public Connection conectar(){
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            cn = (Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/serviciofinanciero", "root", "jmawyin");
+            System.out.println("CONECTADO");
+        }catch(Exception e){
+            System.out.println("ERROR DE CONEXION BD "+e);
+        }
+        return cn;
+    }
+}
---
+++ PROYECTO/src/proyecto/PROYECTO.java
@@ -0,0 +1,12 @@
+package proyecto;
+
+import conexion.*;
+
+public class PROYECTO {
+
+    public static void main(String[] args) {
+        Conexion conecctar = new Conexion();
+        conecctar.conectar();
+    }
+
+}
